72db6dd4e801f2de73c5d076cad45bf33d2e7620
Josias NTUAREMBA <josiasdan@hotmail.fr>
2018-08-24 15:58:02 +0200

    Front de gestion de nature de mission terminé, ajout de nouvelles attribut, ajout de nouvelles fonction au service avec JAVADOC

---
+++ src/app/domains.ts
@@ -0,0 +1,11 @@
+/**
+ * Creation de l'objet Nature de Mission
+ */
+export class NatureMission
+{
+    constructor(public id:number,public libelle:string,public facture:string,public versementPrime:string,public tjm:number,public pourcentage:number,public expiration:Date)
+    {
+   
+    }
+
+}
